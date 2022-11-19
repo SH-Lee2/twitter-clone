44d8912ded4b62b228c7acfd42bef5011262c0be
이시형 <tlgud585@gmail.com>
2022-11-20 03:00:11 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+	return (
+		<div className="">
+			<Head>
+				<title>Twitter clone</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				{/* Sidebar */}
+				{/* Feed */}
+				{/* Widgets */}
+			</main>
+		</div>
+	);
+};
+
+export default Home;
