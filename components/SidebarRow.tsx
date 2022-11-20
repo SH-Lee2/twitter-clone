3db3f5b0fa6417cded3a87e13777388e251e3f36
이시형 <tlgud585@gmail.com>
2022-11-20 19:28:08 +0900

    Style: responsive sidebar

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -12,7 +12,9 @@ const SidebarRow = ({ Icon, title }: Props) => {
     px-4 py-3 rounded-full transition-all duration-200 cursor-pointer group hover:bg-gray-100"
 		>
 			<Icon className="h-6 w-6" />
-			<p className="group-hover:text-twitter">{title}</p>
+			<p className="hidden text-base font-light md:inline-flex lg:text-xl group-hover:text-twitter">
+				{title}
+			</p>
 		</div>
 	);
 };
