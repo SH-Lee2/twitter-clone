089da7e88728739c9def23aba2f06655ffe78fbf
이시형 <tlgud585@gmail.com>
2022-11-20 20:17:52 +0900

    Add: tweetBox UI 추가 트위터 글쓰기

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import TweetBox from "./TweetBox";
 
 const Feed = () => {
 	return (
@@ -9,6 +10,8 @@ const Feed = () => {
 				<h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
 				<ArrowPathIcon className="h-8 w-8 mr-5 mt-5 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
 			</div>
+
+			<TweetBox />
 		</div>
 	);
 };
