1910c0c5d42a15d902d21eac5e5edb7fb13a60dd
이시형 <tlgud585@gmail.com>
2022-11-20 19:27:35 +0900

    Add: Feed UI & widgets  추가

---
+++ components/Widgets.tsx
@@ -0,0 +1,24 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+const Widgets = () => {
+	return (
+		<div className="hidden mt-2 px-2 lg:inline col-span-2">
+			<div className="flex items-center space-x-2 rounded-full p-3 my-2 bg-gray-100">
+				<MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+				<input
+					className="bg-transparent flex-1 outline-none"
+					type="text"
+					placeholder="Search Twitter"
+				/>
+			</div>
+			<TwitterTimelineEmbed
+				sourceType="profile"
+				screenName="elonmusk"
+				options={{ height: 1000 }}
+			/>
+		</div>
+	);
+};
+
+export default Widgets;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
 const Home: NextPage = () => {
 	return (
-		<div className="">
+		<div className="mx-auto max-h-screen overflow-y-hidden lg:max-w-7xl">
 			<Head>
 				<title>Twitter clone</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main>
+			<main className="grid grid-cols-9">
 				<Sidebar />
-				{/* Feed */}
-				{/* Widgets */}
+
+				<Feed />
+
+				<Widgets />
 			</main>
 		</div>
 	);
