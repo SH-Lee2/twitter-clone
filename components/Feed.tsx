9db372cd8d8ec5a7fe4c9065b9a5629bd675c215
이시형 <tlgud585@gmail.com>
2022-11-22 01:55:02 +0900

    Add: toast & refreshTweets
    피드 새로고침 및  로딩, 성공 표시 추가

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { toast } from "react-hot-toast";
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import TweetBox from "./TweetBox";
 import { Tweet } from "../typings";
 import TweetComponent from "./TweetComponent";
+import { fetchTweets } from "../utils/fetchTweets";
 
 interface Props {
 	tweets: Tweet[];
 }
 
-const Feed = ({ tweets }: Props) => {
+const Feed = ({ tweets: tweetsProp }: Props) => {
+	const [tweets, setTweets] = useState<Tweet[]>(tweetsProp);
+
+	const handleRefresh = async () => {
+		const refreshToast = toast.loading("Refreshing...");
+		const tweets = await fetchTweets();
+		setTweets(tweets);
+
+		toast.success("Feed Updated!", {
+			id: refreshToast,
+		});
+	};
+
 	return (
 		<div className="col-span-7 border-x lg:col-span-5">
 			<div className="flex items-center justify-between">
 				<h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
-				<ArrowPathIcon className="h-8 w-8 mr-5 mt-5 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125" />
+				<ArrowPathIcon
+					onClick={handleRefresh}
+					className="h-8 w-8 mr-5 mt-5 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125"
+				/>
 			</div>
 
 			<div>
