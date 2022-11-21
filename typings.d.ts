ea3ec86d264e9cec7c93b6313681d6962a46b78f
이시형 <tlgud585@gmail.com>
2022-11-21 16:02:16 +0900

    Add: type 정의

---
+++ typings.d.ts
@@ -0,0 +1,15 @@
+export interface Tweet extends TweetBody {
+	_id: string;
+	_createAt: string;
+	_updateAt: string;
+	_rev: string;
+	_type: "tweet"; // 참조하는 스키마 이름
+	blockTweet: boolean;
+}
+
+export type TweetBody = {
+	text: string;
+	username: string;
+	profileImg: string;
+	image?: string;
+};
